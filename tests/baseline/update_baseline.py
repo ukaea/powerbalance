f88edf508b95a170f46c3e389ff209c5a42f0621
Kristian Zarębski <kristian.zarebski@ukaea.uk>
2022-02-28 11:39:53 +0000

    Run linter

--- tests/baseline/update_baseline.py
+++ tests/baseline/update_baseline.py
@@ -2,21 +2,18 @@
 """ 
 Update Regression Test Baseline Files
 """
-import pickle
+import glob
 import os.path
 import pathlib
-import numpy as np
-import glob
+import pickle
 
+import numpy as np
 import power_balance.calc.efficiencies as pbm_eff
 import power_balance.profiles as pbm_prof
 
 BASELINE_DIR = os.path.dirname(__file__)
 PROFILES_DIR = os.path.join(
-    pathlib.Path(__file__).parents[2],
-    "power_balance",
-    "profiles",
-    "mat_profile_files"
+    pathlib.Path(__file__).parents[2], "power_balance", "profiles", "mat_profile_files"
 )
 
 
@@ -25,14 +22,8 @@ def update_pckl_baseline():
         os.path.join(BASELINE_DIR, "default_heating2elec_eff.pckl"),
         os.path.join(BASELINE_DIR, "default_thermal2elec_eff.pckl"),
     )
-    eff_funcs = (
-        "calc_heating_to_elec_eff",
-        "calc_thermal_to_elec_eff"
-    )
-    keys = (
-        "heating_profile",
-        "thermal_in_profile"
-    )
+    eff_funcs = ("calc_heating_to_elec_eff", "calc_thermal_to_elec_eff")
+    keys = ("heating_profile", "thermal_in_profile")
     for pckl_f, func, key in zip(pickle_files, eff_funcs, keys):
         _data = pickle.load(open(pckl_f, "rb"))
         _tmp = _data.copy()
